c222063d8d0cefde161826ec784a14d0fd6263ae
jabermudez51-lab <jabermudez51@uan.edu.co>
2026-04-13 22:21:59 -0500

    Update Section.kt

--- app/src/main/java/co/edu/uan/android/uancasts/ui/screens/passwords/Section.kt
+++ app/src/main/java/co/edu/uan/android/uancasts/ui/screens/passwords/Section.kt
@@ -1,31 +1,36 @@
-@Composable
+package co.edu.uan.android.uancasts.ui.screens.passwords
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import co.edu.uan.android.passgo.R
 
+@Composable
 fun Section(title: String) {
-
     Column {
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(title, color = Color.White, fontSize = 14.sp)
+            Text(
+                text = title,
+                color = Color.White,
+                fontSize = 14.sp
+            )
 
             Spacer(modifier = Modifier.weight(1f))
 
             Text(
-                "Ver todo",
+                text = "Ver todo",
                 color = Color(0xFF42A5F5),
                 fontSize = 12.sp
             )
@@ -33,20 +38,9 @@ fun Section(title: String) {
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        PasswordItem(
-            name = "Figma",
-            icon = R.drawable.figma
-        )
-
-        PasswordItem(
-            name = "Facebook",
-            icon = R.drawable.facebook
-        )
-
-        PasswordItem(
-            name = "Instagram",
-            icon = R.drawable.instagram
-        )
+        PasswordItem(name = "Figma")
+        PasswordItem(name = "Facebook")
+        PasswordItem(name = "Instagram")
 
         Spacer(modifier = Modifier.height(16.dp))
     }
