90307a6bf4173d9d6ec9f69fa83b726157daf1df
jabermudez51-lab <jabermudez51@uan.edu.co>
2026-05-03 23:16:09 -0500

    Update MainActivity.kt

--- app/src/main/java/co/edu/uan/android/passgo/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/passgo/MainActivity.kt
@@ -3,15 +3,21 @@ package co.edu.uan.android.passgo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import co.edu.uan.android.passgo.data.viewmodel.PassGoViewModel
 import co.edu.uan.android.passgo.navigation.AppNavigation
 import co.edu.uan.android.passgo.ui.theme.UANCastsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val passGoViewModel: PassGoViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             UANCastsTheme {
-                AppNavigation()
+                AppNavigation(viewModel = passGoViewModel)
             }
         }
     }
